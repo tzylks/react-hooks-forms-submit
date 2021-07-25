1f7830985abb2a13efb34ab5ef467517b57e921a
TrevorZylks <trevorzylks@gmail.com>
2021-07-24 17:41:29 -0700

    Complete

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,12 @@
+
 import React, { useState } from "react";
 
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
+  const [submittedData, setSubmittedData] = useState([]);
+  const [errors, setErrors] = useState([]);
+
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,13 +16,48 @@ function Form() {
     setLastName(event.target.value);
   }
 
-  return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
+  function handleSubmit(event) {
+    event.preventDefault();
+    // first name is required
+    if (firstName.length > 0) {
+      const formData = { firstName: firstName, lastName: lastName };
+      const dataArray = [...submittedData, formData];
+      setSubmittedData(dataArray);
+      setFirstName("");
+      setLastName("");
+      setErrors([]);
+    } else {
+      setErrors(["First name is required!"]);
+    }
+  } 
 
-export default Form;
+  const listOfSubmissions = submittedData.map((data, index) => {
+    return (
+      <div key={index}>
+        {data.firstName} {data.lastName}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+      {/* conditionally render error messages */}
+      {errors.length > 0
+        ? errors.map((error, index) => (
+            <p key={index} style={{ color: "red" }}>
+              {error}
+            </p>
+          ))
+        : null}
+      <h3>Submissions</h3>
+      {listOfSubmissions}
+    </div>
+  ); 
+        }
+
+export default Form; 
